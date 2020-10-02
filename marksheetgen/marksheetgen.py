d43f9bdeafd76c0e33df35bec0e735093075cd77
Haggen So <haggenso@gmail.com>
2020-10-02 12:39:21 +0800

    Add comment to marksheetgen

--- marksheetgen/marksheetgen.py
+++ marksheetgen/marksheetgen.py
@@ -1,11 +1,20 @@
+"""
+This program read in class roster from the file "classroster.csv" with the format:
+
+StudID	StudName	Class
+A001	Name1	A
+
+The Excel template is in the file "marksheet_template.xlsx". 
+
+"""
+
 from openpyxl import load_workbook
 from openpyxl import Workbook
 import pandas as pd
-import copy
 
 # Read Student Roster
 df = pd.read_csv("classroster.csv")
-#print(df)
+# print(df)
 
 # Loop
 for index, row in df.iterrows():
@@ -14,7 +23,7 @@ for index, row in df.iterrows():
 	# Read xlsx
 	wb = load_workbook(filename = 'marksheet_template.xlsx')
 	ws = wb.active
-	#print(ws)
+	# print(ws)
 
 	ws['B1']=row['StudName']
 	ws['B2']=row['StudID']
