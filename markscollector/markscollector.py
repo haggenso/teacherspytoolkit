f473bdfcb64bbce19788c3762e99f05a5bcfa746
Haggen So <haggenso@gmail.com>
2024-07-26 14:37:41 +1000

    Update comments in marksheetcollector

--- markscollector/markscollector.py
+++ markscollector/markscollector.py
@@ -1,6 +1,7 @@
-# openpyxl can only read the value of very simple calcuated cell
-# Even =SUM(...) does not work :'(
-# Ref: https://stackoverflow.com/questions/23350581/openpyxl-1-8-5-reading-the-result-of-a-formula-typed-in-a-cell-using-openpyxl
+"""
+Collect student details and marks from all the *.xlsx files in a directory
+Using xlwings library that can read calculate results from formulae
+"""
 
 import xlwings as xw
 import pandas as pd
